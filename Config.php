ad52ac0a8299fdea071b92df98af38ef0571de4a
余亮 <wudy.yu@huolala.cn>
2019-05-25 13:30:37 +0800

    提交mysqli库

--- Config.php
+++ Config.php
@@ -38,5 +38,21 @@ return [
         'nodeTimeout'=>10,
         'nodeName'=>'easySwoole',
         'nodeId'=>null
+    ],
+    'MYSQL' => [
+        //数据库配置
+        'host'                 => '127.0.0.1',//数据库连接ip
+        'user'                 => 'root',//数据库用户名
+        'password'             => '000000',//数据库密码
+        'database'             => 'blog',//数据库
+        'port'                 => '3306',//端口
+        'timeout'              => '30',//超时时间
+        'connect_timeout'      => '5',//连接超时时间
+        'charset'              => 'utf8',//字符编码
+        'strict_type'          => false, //开启严格模式，返回的字段将自动转为数字类型
+        'fetch_mode'           => false,//开启fetch模式, 可与pdo一样使用fetch/fetchAll逐行或获取全部结果集(4.0版本以上)
+        'alias'                => '',//子查询别名
+        'isSubQuery'           => false,//是否为子查询
+        'max_reconnect_times ' => '3',//最大重连次数
     ]
 ];
