954433e16b4bc86e6c4fc3dbe3b789e8a27b051a
余亮 <wudy.yu@huolala.cn>
2019-05-26 08:50:45 +0800

    MysqliDb方式

--- App/HttpController/Api/Index.php
+++ App/HttpController/Api/Index.php
@@ -8,6 +8,10 @@
 
 namespace App\HttpController\Api;
 
+use EasySwoole\Core\Component\Di;
+
+
+
 class Index extends Base
 {
     //子类Index继承自抽象父类Controller, 父类中有抽象方法abstract index() , 那么子类也必须实现该index方法
@@ -18,14 +22,22 @@ class Index extends Base
 //    }
 
     //http://wudy.easyswoole.cn:8090/api/video?age=0
+    //http://wudy.easyswoole.cn:8090/api/index/video
     public function video()
     {
-        $data = [
-            'name' => 'wudy',
-            'age' => $this->request()->getRequestParam('age'),
-            'params' => $this->request()->getRequestParam(),
+//        $data = [
+//            'name' => 'wudy',
+//            'age' => $this->request()->getRequestParam('age'),
+//            'params' => $this->request()->getRequestParam(),
+//
+//        ];
+//        return $this->writeJson(200, 'ok', $data);
 
-        ];
-        return $this->writeJson(200, 'ok', $data);
+        //查询数据库(MysqliDb方式)
+        //在mainServerCreate方法中已经注入过了MYSQL
+        $db = Di::getInstance()->get('MYSQL');
+        $result = $db->where('id', 1)->getOne('video');
+//        print_r($result);
+        return $this->writeJson(200, 'ok', $result);
     }
 }
--- EasySwooleEvent.php
+++ EasySwooleEvent.php
@@ -9,6 +9,7 @@
 namespace EasySwoole;
 
 use \EasySwoole\Core\AbstractInterface\EventInterface;
+use EasySwoole\Core\Component\Di;
 use \EasySwoole\Core\Swoole\ServerManager;
 use \EasySwoole\Core\Swoole\EventRegister;
 use \EasySwoole\Core\Http\Request;
@@ -22,9 +23,28 @@ Class EasySwooleEvent implements EventInterface {
         date_default_timezone_set('Asia/Shanghai');
     }
 
+    /**
+     * @param ServerManager $server
+     * @param EventRegister $register
+     * 主服务创建事件
+     * 在执行该事件的时候，已经完成的工作有:
+     * 1.框架初始化事件
+     * 2.配置文件加载完成
+     * 3.主Swoole Server创建成功
+     * 4.主Swoole Server 注册了默认的onRequest,onTask,onFinish事件
+     */
     public static function mainServerCreate(ServerManager $server,EventRegister $register): void
     {
-        // TODO: Implement mainServerCreate() method.
+        //mysql相关
+        //Di 依赖注入
+        Di::getInstance()->set('MYSQL', \MysqliDb::class, Array(
+            'host' => '127.0.0.1',
+            'user' => 'root',
+            'password' => '000000',
+            'db' => 'blog',
+            'port' => 3306,
+            'charset' => 'utf8',
+        ));
     }
 
     public static function onRequest(Request $request,Response $response): void
